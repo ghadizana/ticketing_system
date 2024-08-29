1145d5cf0306bcd8cefe6834b6d360cb116b9acc
ghadizana <ghadizanauraaliya@gmail.com>
2024-08-29 17:09:51 +0700

    image for profile

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -11,7 +12,7 @@ use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable, HasRoles;
+    use HasApiTokens, HasFactory, Notifiable, HasRoles, SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
@@ -58,5 +59,9 @@ class User extends Authenticatable
 
     public function komentars() {
         return $this->hasMany(Komentar::class, 'idKomentar', 'idKomentar');
+    }
+
+    public function profile() {
+        return $this->hasOne(Profile::class, 'userId');
     }
 }
